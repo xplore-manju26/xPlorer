5bcdcbd18143077a372bedceb7529a66b3b3444d
Manju <xplore.manju26@gmail.com>
2023-12-14 22:05:49 +0000

    New Commit

---
+++ gLory/curiocity.py
@@ -0,0 +1,21 @@
+def incrementer() :
+    i = 1
+    while True :
+        yield i
+        i += 1
+
+inc = incrementer()
+print( next( inc ) )
+print( next( inc ) )
+print( next( inc ) )
+
+for i in inc:
+        if (i<30):
+            print (i)
+        else:
+             break
+
+
+
+
+
